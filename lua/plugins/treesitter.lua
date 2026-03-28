0c9dac7d924153b6e3aa930880e608ceba5f6bae
Joshua Lin <joshua@lintropic.com>
2026-03-28 13:20:26 -0400

    aerial working

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.config").setup({
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
