3684228c45f92dfcf76510265445be8166c45da6
Joshua Lin <research@joshualin.io>
2026-04-22 13:01:39 -0400

    checkpoint

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -21,16 +21,102 @@ vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle fil
 vim.keymap.set("n", "<leader>T", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file tree" })
 vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>", { desc = "Toggle Aerial" })
 
--- Interactive execution (Molten) 
+-- Interactive execution (Molten)
 keymap("n", "<leader>mi", "<cmd>MoltenInit<CR>", { desc = "Molten init" })
-
 keymap("n", "<leader>ml", "<cmd>MoltenEvaluateLine<CR>", { desc = "Molten eval line" })
-keymap("v", "<leader>mv", "<cmd><C-u>MoltenEvaluateVisual<CR>", { desc = "Molten eval visual" })
-
+keymap("v", "<leader>mv", "<cmd>MoltenEvaluateVisual<CR>", { desc = "Molten eval visual" })
 keymap("n", "<leader>mo", "<cmd>MoltenEvaluateOperator<CR>", { desc = "Molten eval operator" })
 
-keymap("n", "<leader>mr", "<cmd>MoltenReevaluateCell<CR>", { desc = "Molten reevaluate cell" })
-keymap("n", "<leader>md", "<cmd>MoltenDelete<CR>", { desc = "Molten delete cell" })
+local function is_percent_marker(line)
+  return line:match("^%s*#%s*%%%%%s*$") ~= nil
+end
 
-keymap("n", "<leader>mx", "<cmd>MoltenInterrupt<CR>", { desc = "Molten interrupt" })
-keymap("n", "<leader>mR", "<cmd>MoltenRestart<CR>", { desc = "Molten restart kernel" })
+local function molten_eval_percent_cell()
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  local last = vim.api.nvim_buf_line_count(0)
+
+  local start_row = 1
+  for l = row - 1, 1, -1 do
+    if is_percent_marker(vim.fn.getline(l)) then
+      start_row = l + 1
+      break
+    end
+  end
+
+  local end_row = last
+  for l = row + 1, last do
+    if is_percent_marker(vim.fn.getline(l)) then
+      end_row = l - 1
+      break
+    end
+  end
+
+  if start_row > end_row then
+    return
+  end
+
+  vim.fn.setpos("'<", { 0, start_row, 1, 0 })
+  vim.fn.setpos("'>", { 0, end_row, 1, 0 })
+  vim.cmd("normal! gv")
+  vim.cmd("MoltenEvaluateVisual")
+end
+
+local function molten_cell_ranges()
+  local last = vim.api.nvim_buf_line_count(0)
+  local ranges = {}
+  local start_row = 1
+
+  for l = 1, last do
+    if is_percent_marker(vim.fn.getline(l)) then
+      if start_row <= l - 1 then
+        table.insert(ranges, { start_row, l - 1 })
+      end
+      start_row = l + 1
+    end
+  end
+
+  if start_row <= last then
+    table.insert(ranges, { start_row, last })
+  end
+
+  return ranges
+end
+
+local function molten_run_all_percent_cells()
+  for _, range in ipairs(molten_cell_ranges()) do
+    vim.fn.MoltenEvaluateRange(range[1], range[2])
+  end
+end
+
+local function molten_restart_and_run_all()
+  local group = vim.api.nvim_create_augroup("MoltenRestartRunAll", { clear = true })
+
+  vim.api.nvim_create_autocmd("User", {
+    group = group,
+    pattern = "MoltenKernelReady",
+    once = true,
+    callback = molten_run_all_percent_cells,
+  })
+
+  vim.cmd("MoltenRestart!")
+end
+
+keymap("n", "<leader>mc", molten_eval_percent_cell, {
+  desc = "Molten eval #%% cell",
+})
+
+keymap("n", "<leader>mr", "<cmd>MoltenReevaluateCell<CR>", {
+  desc = "Molten reevaluate cell",
+})
+
+keymap("n", "<leader>md", "<cmd>MoltenDelete<CR>", {
+  desc = "Molten delete cell",
+})
+
+keymap("n", "<leader>mx", "<cmd>MoltenInterrupt<CR>", {
+  desc = "Molten interrupt",
+})
+
+keymap("n", "<leader>mR", molten_restart_and_run_all, {
+  desc = "Molten restart kernel and run all #%% cells",
+})
