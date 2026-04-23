98278dc6b0a086c64a9a53bfa96a399b42351fee
Joshua Lin <research@joshualin.io>
2026-04-23 17:14:50 -0400

    theme carbonfox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 -- ~/.config/nvim/lua/plugins/colorscheme.lua
 return {
   {
-    -- "EdenEast/nightfox.nvim",
+    "EdenEast/nightfox.nvim",
     -- "folke/tokyonight.nvim",
-    "catppuccin/nvim", name="catppuccin", 
+    -- "catppuccin/nvim", name="catppuccin", 
     lazy = false,        -- make sure we load this during startup
     priority = 1000,     -- make sure to load this before other plugins
     opts = {},
