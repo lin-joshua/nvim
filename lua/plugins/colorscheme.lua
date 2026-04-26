de8ae644a38647332997abe83cc86defe4221ed7
Joshua Lin <research@joshualin.io>
2026-04-26 17:49:11 -0400

    simplified

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,14 @@
 -- ~/.config/nvim/lua/plugins/colorscheme.lua
 return {
   {
-    "EdenEast/nightfox.nvim",
-    "folke/tokyonight.nvim",
+    -- "EdenEast/nightfox.nvim",
+    -- "folke/tokyonight.nvim",
     "catppuccin/nvim", name="catppuccin", 
+    -- "Mofiqul/dracula.nvim",
     lazy = false,        -- make sure we load this during startup
     priority = 1000,     -- make sure to load this before other plugins
-    opts = {},
+    opts = {
+        colorscheme = "catppuccin"
+    },
   },
 }
