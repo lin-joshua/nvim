4ad5dfd364882078d72e9d20797ef6ed5c968284
Joshua Lin <research@joshualin.io>
2026-04-30 18:02:45 -0400

    numeric jumping uses logical not visual line numbers

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,10 +5,10 @@ vim.g.maplocalleader = "\\"
 local keymap = vim.keymap.set
 
 -- Have j and k navigate visual lines rather than logical ones
-keymap("n", "j", "gj")
-keymap("n", "k", "gk")
-keymap("v", "j", "gj")
-keymap("v", "k", "gk")
+keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+keymap("v", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+keymap("v", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
 
 -- Use H and L for beginning/end of line
 keymap("n", "H", "^")
@@ -20,4 +20,3 @@ keymap("v", "L", "$")
 vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree" })
 vim.keymap.set("n", "<leader>T", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file tree" })
 vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>", { desc = "Toggle Aerial" })
-
